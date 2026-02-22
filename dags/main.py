9a0f7f70896c1a6eeb568af356fc0c5c4a28875c
Amritpal985 <as0706248@gmail.com>
2026-02-22 19:59:50 +0530

    update_db DAG

--- dags/main.py
+++ dags/main.py
@@ -1,18 +1,22 @@
 from airflow import DAG
-# from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+import pendulum
 from datetime import datetime, timedelta
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-import pendulum
-from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
-from dataquality.soda import yt_elt_data_quality
 
+from api.video_stats import (
+    get_playlist_id,
+    get_video_ids,
+    extract_video_data,
+    save_to_json,
+)
 
 from datawarehouse.dwh import staging_table, core_table
-# from dataquality.soda import yt_elt_data_quality
+from dataquality.soda import yt_elt_data_quality
 
+# Define the local timezone
+local_tz = pendulum.timezone("Europe/Malta")
 
-local_tz = pendulum.timezone("Asia/Kolkata")
-
+# Default Args
 default_args = {
     "owner": "dataengineers",
     "depends_on_past": False,
@@ -27,16 +31,18 @@ default_args = {
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
 
+# Variables
 staging_schema = "staging"
 core_schema = "core"
 
+# DAG 1: produce_json
 with DAG(
     dag_id="produce_json",
     default_args=default_args,
     description="DAG to produce JSON file with raw data",
     schedule="0 14 * * *",
-    catchup=False
-) as dag:
+    catchup=False,
+) as dag_produce:
 
     # Define tasks
     playlist_id = get_playlist_id()
@@ -50,16 +56,18 @@ with DAG(
     )
 
     # Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task>> trigger_update_db
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
-
+# DAG 2: update_db
 with DAG(
     dag_id="update_db",
     default_args=default_args,
-    description="DAG to update database with raw data",
-    schedule="0 15 * * *",
-    catchup=False
-) as dag:
+    description="DAG to process JSON file and insert data into both staging and core schemas",
+    catchup=False,
+    schedule=None,
+) as dag_update:
+
+    # Define tasks
     update_staging = staging_table()
     update_core = core_table()
 
@@ -68,8 +76,8 @@ with DAG(
         trigger_dag_id="data_quality",
     )
 
+    # Define dependencies
     update_staging >> update_core >> trigger_data_quality
-
 
 # DAG 3: data_quality
 with DAG(

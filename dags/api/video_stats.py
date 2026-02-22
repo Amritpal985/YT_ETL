f5845a2fc4996b685600ccf0c4d7ee0e8c3659c1
Amritpal985 <as0706248@gmail.com>
2026-02-22 18:19:31 +0530

    Updated code for ETL

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,16 +1,20 @@
 import requests
 import json
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 from datetime import date, date
 
+from airflow.decorators import task
+from airflow.models import Variable
 
-load_dotenv(dotenv_path="./.env")
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+# load_dotenv(dotenv_path="./.env")
+
+API_KEY =  Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
+@task
 def get_playlist_id():
     try:
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
@@ -25,8 +29,7 @@ def get_playlist_id():
         raise e
 
 
-
-
+@task
 def get_video_ids(playlistId):
     video_ids = []
     pageToken = None 
@@ -55,7 +58,7 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
     def batch_list(video_id_list,batch_size):
@@ -76,19 +79,20 @@ def extract_video_data(video_ids):
                 contentDetails= item['contentDetails']
                 statistics = item['statistics']
                 video_data = {
-                    "video_id":video_id,
+                    "video_id": video_id,
                     "title": snippet["title"],
-                    "publishedAt":snippet["publishedAt"],
-                    "duration": contentDetails['duration'],
-                    "viewcount": statistics.get('viewcount',None),
-                    "Commentcount":statistics.get('commentCount',None)
+                    "publishedAt": snippet["publishedAt"],
+                    "duration": contentDetails["duration"],
+                    "viewCount": statistics.get("viewCount", None),
+                    "likeCount": statistics.get("likeCount", None),
+                    "commentCount": statistics.get("commentCount", None),
                 }
                 extracted_data.append(video_data)
         return extracted_data
 
     except requests.exceptions.RequestException as e:
         raise e
-
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
     with open(file_path, "w", encoding="utf-8") as f:
